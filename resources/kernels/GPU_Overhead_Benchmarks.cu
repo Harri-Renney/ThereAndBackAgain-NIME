ae0bb99dadc7e754fdcc22b8f12861ba0af730e6
h2-renney <harri2.renney@live.uwe.ac.uk>
2019-12-14 14:51:20 +0000

    Beginning CUDA kernels.

---
+++ resources/kernels/GPU_Overhead_Benchmarks.cu
@@ -0,0 +1,18 @@
+__device__
+void nullKernel()
+{
+
+}
+
+__device__
+void copyBuffer(float *C, float *A, float *B, size_type wA,
+                          size_type wB)
+{
+
+}
+
+__device__
+void copyBuffer(float* gridOne, float* gridTwo, float* gridThree, float* boundaryGain, int samplesIndex, float* samples, float* excitation, int listenerPosition, int excitationPosition, float propagationFactor, float dampingFactor, int rotationIndex)
+{
+
+}
